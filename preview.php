e241ee8eb133865e6b68fe98bfa6a288b5744857
Eoin Campbell <campbeeo@tcd.ie>
2010-09-11 15:31:10 +0000

    Fix preview facility when Moodle installed in subfolder

--- preview.php
+++ preview.php
@@ -10,9 +10,6 @@ require_once($CFG->libdir.'/moodlelib.php');
 require_once($CFG->libdir.'/questionlib.php');
 require_once($CFG->libdir.'/dmllib.php');
 
-// Include utility functions
-require_once('mqutility.php');
-
 // Get the assigned temporary question name
 $qname = required_param('qname', PARAM_TEXT);
 // Get the course id
@@ -20,8 +17,11 @@ $courseid = required_param('courseid', PARAM_INT);
 
 // Get the question ID by searching for the unique name, and redirect to the preview page
 if(($question = get_record('question', 'name', $qname))) {
-	$redirect_url = "/question/preview.php?id=" . $question->id . "&courseid=" . $courseid;
-	redirect($redirect_url);
+    // Figure out the proper URL, allowing for an installation in a subfolder
+    $moodle_root_folder_path = parse_url($CFG->wwwroot, PHP_URL_PATH);
+    $redirect_url = $moodle_root_folder_path . "/question/preview.php?id=" . $question->id . "&courseid=" . $courseid;
+    redirect($redirect_url);
+} else {   // No question found, report an error message so the reader isn't looking at a blank screen
+    notify(get_string('preview_question_not_found', 'qformat_wordtable', $qname . " / " . $courseid));
 }
-// Should handle case where question not found and return a suitable error.
 ?>
