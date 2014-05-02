1703cf7a7f006c7e457518c27d54857a7a5de46d
Eoin Campbell <campbeeo@tcd.ie>
2014-05-02 12:45:31 +0100

    CONTRIB-5028: clean up HTML markup better using if tidy not available

--- format.php
+++ format.php
@@ -437,7 +437,6 @@ class qformat_wordtable extends qformat_xml {
         $n_questions = count($question_matches);
         if ($found_questions === FALSE or $found_questions == 0) {
             //debugging(__FUNCTION__ . ":" . __LINE__ . ": Cannot decompose questions", DEBUG_DEVELOPER);
-            echo $OUTPUT->notification(get_string('noquestions', 'qformat_wordtable'));
             return $input_string;
         }
         //debugging(__FUNCTION__ . ":" . __LINE__ . ": " . $n_questions . " questions found", DEBUG_DEVELOPER);
@@ -456,7 +455,6 @@ class qformat_wordtable extends qformat_xml {
                 $n_text_fields = count($cdata_matches);
                 if ($found_text_fields === FALSE or $found_text_fields == 0) {
                     //debugging(__FUNCTION__ . ":" . __LINE__ . ": Cannot decompose text elements in question", DEBUG_DEVELOPER);
-                    echo $OUTPUT->notification(get_string('noquestions', 'qformat_wordtable'));
                     return $input_string;
                 }
 
@@ -488,31 +486,40 @@ class qformat_wordtable extends qformat_xml {
     /**
      * Clean HTML content
      *
-     * A string containing clean HTML is returned
+     * A string containing clean XHTML is returned
      *
      * @return string
      */
     private function clean_html_text($text_content_string) {
+        $tidy_type = "strip_tags";
 
         // Check if Tidy extension loaded, and use it to clean the CDATA section if present
         if (extension_loaded('tidy')) {
             // cf. http://tidy.sourceforge.net/docs/quickref.html
+            $tidy_type = "tidy";
             $tidy_config = array(
                 'bare' => true, // Strip Microsoft Word 2000-specific markup
                 'clean' => true, // Replace presentational with structural tags 
                 'word-2000' => true, // Strip out other Microsoft Word gunk
                 'drop-font-tags' => true, // Discard font
                 'drop-proprietary-attributes' => true, // Discard font
+                'output-xhtml' => true, // Output XML, to format empty elements properly
                 'show-body-only'   => true,
             );
             $clean_html = tidy_repair_string($text_content_string, $tidy_config, 'utf8');
-        } else { // Tidy not available, so just strip most HTML tags except bold, italic, subscript and superscript
-            $clean_html = strip_tags($text_content_string, "<a><b><i><sub><sup>");
+        } else { 
+            // Tidy not available, so just strip most HTML tags except character-level markup and table tags
+            $clean_html = strip_tags($text_content_string, "<b><br><em><i><img><strong><sub><sup><u><table><tbody><td><th><thead>");
+
+            // The strip_tags function treats empty elements like HTML, not XHTML, so fix <br> and <img src=""> manually (i.e. <br/>, <img/>)
+            $clean_html = preg_replace('~<img([^>]*?)/?>~si', '<img$1/>', $clean_html, PREG_SET_ORDER);
+            $clean_html = preg_replace('~<br([^>]*?)/?>~si', '<br/>', $clean_html, PREG_SET_ORDER);
         }
 
         // Strip soft hyphens (0xAD, or decimal 173)
         $clean_html = preg_replace('/\xad/u', '', $clean_html);
-            debugging(__FUNCTION__ . "() strip_tags -> |" . $clean_html . "|", DEBUG_DEVELOPER);
+
+        debugging(__FUNCTION__ . "() [using " . $tidy_type . "] -> |" . $clean_html . "|", DEBUG_DEVELOPER);
         return $clean_html;
     }
 }
