7ef9d4bda73e17a40d49572abb7a3517af0fe2c3
Eoin Campbell <campbeeo@tcd.ie>
2014-05-03 14:08:00 +0100

    Handle image names containing spaces

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
  * @package wordtable
  **/
 
-$plugin->version  = 2014050102;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2014050301;  // The current module version (Date: YYYYMMDDXX)
 $plugin->maturity  = MATURITY_STABLE;  // Maturity level
 $plugin->component  = 'qformat_wordtable';  // Plugin name
-$plugin->release  = '2.8.1 (Build: 20140502)';  // The current module release in human-readable form (x.y)
+$plugin->release  = '2.8.2 (Build: 20140503)';  // The current module release in human-readable form (x.y)
 $plugin->requires = 2011070100.03;  // Requires Moodle 2.1 or later
 $plugin->cron     = 0;           // Period for cron to check this module (secs)
 
