c431e0db742e6faf7a5e982a009e56bcbb74c325
Eoin Campbell <campbeeo@tcd.ie>
2015-08-14 12:26:21 +0100

    Fix early close error with Word file

--- format.php
+++ format.php
@@ -214,11 +214,11 @@ class qformat_wordtable extends qformat_xml {
                 }
                 // Get the next file in the Zip package
                 $zip_entry = zip_read($zfh);
-            zip_close($zfh);
             }  // End while
+            zip_close($zfh);
         } else { // Can't open the Word .docx file for reading
             echo $OUTPUT->notification(get_string('cannotopentempfile', 'qformat_wordtable', $filename));
-            $this->debug_unlink($zipfile);
+            $this->debug_unlink($filename);
             return false;
         }
 
