f087afe212374d9a63cf3aae9c81563f49e1c642
Eoin Campbell <campbeeo@tcd.ie>
2015-08-21 15:44:19 +0100

    Use DOMDocument to convert HTML into XHTML, avoiding the need for Tidy, and better results than strip_tags

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later (5)
  **/
 
-$plugin->version  = 2015081902;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2015082101;  // The current module version (Date: YYYYMMDDXX)
 $plugin->maturity  = MATURITY_STABLE;  // Maturity level
 $plugin->component  = 'qformat_wordtable';  // Plugin name
-$plugin->release  = '3.2.2 (Build: 20150819)';  // The current module release in human-readable form (x.y)
+$plugin->release  = '3.2.3 (Build: 20150821)';  // The current module release in human-readable form (x.y)
 $plugin->requires = 2011070100.03;  // Requires Moodle 2.1 or later
 $plugin->cron     = 0;           // Period for cron to check this module (secs)
 
