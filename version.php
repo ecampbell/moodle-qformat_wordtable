31260ddc62894fd18039b10962b5644a55c9cd90
Eoin Campbell <campbeeo@tcd.ie>
2017-04-17 20:58:56 +0100

    Use canonical reference to temp directory

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later (5)
  **/
 
-$plugin->version   = 2017010501;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017041701;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->maturity  = MATURITY_STABLE;  // Maturity level.
 $plugin->component  = 'qformat_wordtable';  // Plugin name.
-$plugin->release  = '3.5.15 (Build: 20170105)';  // The current module release in human-readable form (x.y).
+$plugin->release  = '3.5.15 (Build: 20170417)';  // The current module release in human-readable form (x.y).
 $plugin->requires = 2011070100.03;  // Requires Moodle 2.1 or later.
 $plugin->cron     = 0;           // Period for cron to check this module (secs).
